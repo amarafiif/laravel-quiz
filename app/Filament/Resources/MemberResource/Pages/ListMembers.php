e0dca1c65951a03c8817e4399b765c05be6791cc
amarafiif <amarafif230@gmail.com>
2025-01-15 00:56:26 +0700

    feat: member resource, filament translation

---
+++ app/Filament/Resources/MemberResource/Pages/ListMembers.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Filament\Resources\MemberResource\Pages;
+
+use App\Filament\Resources\MemberResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListMembers extends ListRecords
+{
+    protected static string $resource = MemberResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make()
+                ->label('Tambah Member')
+                ->icon('heroicon-o-academic-cap')
+        ];
+    }
+}

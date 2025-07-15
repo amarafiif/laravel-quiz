e87d7b74ce324e37cf280c0479aaee4dd114ba8c
amarafiif <amarafif230@gmail.com>
2025-07-15 19:49:32 +0700

    fix: Update button background color for improved visibility in course index

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -30,7 +30,7 @@
                                 <!-- Action Button -->
                                 <div class="flex flex-shrink-0 justify-center">
                                     <a href="{{ route('courses.show', $item->code) }}"
-                                        class="inline-flex items-center rounded-md bg-sky-600 px-4 py-2 text-sm font-medium text-white transition duration-150 ease-in-out hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2">
+                                        class="inline-flex items-center rounded-md bg-sky-500 px-4 py-2 text-sm font-medium text-white transition duration-150 ease-in-out hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2">
                                         Lihat Kuis
                                     </a>
                                 </div>
